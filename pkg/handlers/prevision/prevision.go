ca3325fb987e8cae21edf94bc3eac13859d03d0e
Anderson Freire <anderson_nascimento.16@hotmail.com>
2023-10-12 23:40:35 -0300

    feat: create function for search prevision

--- pkg/handlers/prevision/prevision.go
+++ pkg/handlers/prevision/prevision.go
@@ -1,8 +1,55 @@
 package prevision
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
 
-func GetPrevisionHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Olá, Teste!"))
+	"github.com/andersonnfreire/api-climate/pkg/apis"
+	"github.com/andersonnfreire/api-climate/pkg/config"
+	"github.com/andersonnfreire/api-climate/pkg/handlers/validation"
+)
+
+func GetPrevisionHandler(writerResponse http.ResponseWriter, request *http.Request, cfg config.Config) {
+
+	if err := validation.ValidateQueryParam(request); err != nil {
+		errorMsg := map[string]string{
+			"error": err.Error(),
+		}
+		response, err := json.Marshal(errorMsg)
+		if err != nil {
+			http.Error(writerResponse, "Erro interno do servidor", http.StatusInternalServerError)
+			return
+		}
+		writerResponse.Header().Set("Content-Type", "application/json")
+		writerResponse.WriteHeader(http.StatusUnprocessableEntity)
+		writerResponse.Write(response)
+		return
+	}
+
+	response, err := apis.SendHTTPRequest(apis.HTTPRequestOptions{
+		Method: "GET",
+		URL:    "http://api.openweathermap.org/data/2.5/weather",
+		Headers: map[string]string{
+			"q":     request.URL.Query().Get("cidade"),
+			"appid": request.URL.Query().Get("token"),
+		},
+		Timeout: 5 * time.Minute,
+	})
+
+	writerResponse.Header().Set("Content-Type", "application/json")
+	writerResponse.WriteHeader(response.StatusCode)
+
+	if err != nil {
+		errorMsg := map[string]string{
+			"error": err.Error(),
+		}
+		responseMsg, err := json.Marshal(errorMsg)
+		if err != nil {
+			http.Error(writerResponse, "Erro interno do servidor", http.StatusInternalServerError)
+			return
+		}
+		writerResponse.Write(responseMsg)
+	}
+	writerResponse.Write([]byte(response.Body))
 }
