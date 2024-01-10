877fdb7fe00660b2fd3be17e7be49774cc8ae12f
Anderson Freire <anderson_nascimento.16@hotmail.com>
2024-01-09 22:23:03 -0300

    feat: create function for get api open weather

--- pkg/handlers/prevision/prevision.go
+++ pkg/handlers/prevision/prevision.go
@@ -1,55 +1,22 @@
 package prevision
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/andersonnfreire/api-climate/pkg/apis"
 	"github.com/andersonnfreire/api-climate/pkg/config"
 	"github.com/andersonnfreire/api-climate/pkg/handlers/validation"
 )
 
-func GetPrevisionHandler(writerResponse http.ResponseWriter, request *http.Request, cfg config.Config) {
-
-	if err := validation.ValidateQueryParam(request); err != nil {
-		errorMsg := map[string]string{
-			"error": err.Error(),
-		}
-		response, err := json.Marshal(errorMsg)
-		if err != nil {
-			http.Error(writerResponse, "Erro interno do servidor", http.StatusInternalServerError)
-			return
-		}
-		writerResponse.Header().Set("Content-Type", "application/json")
-		writerResponse.WriteHeader(http.StatusUnprocessableEntity)
-		writerResponse.Write(response)
-		return
-	}
-
-	response, err := apis.SendHTTPRequest(apis.HTTPRequestOptions{
-		Method: "GET",
-		URL:    "http://api.openweathermap.org/data/2.5/weather",
-		Headers: map[string]string{
-			"q":     request.URL.Query().Get("cidade"),
-			"appid": request.URL.Query().Get("token"),
-		},
-		Timeout: 5 * time.Minute,
-	})
-
-	writerResponse.Header().Set("Content-Type", "application/json")
-	writerResponse.WriteHeader(response.StatusCode)
-
+func GetPrevision(writerResponse http.ResponseWriter, request *http.Request, cfg config.Config) (apis.HTTPResponse, error) {
+	params, err := validation.ValidateQueryParamGetPrevision(request)
 	if err != nil {
-		errorMsg := map[string]string{
-			"error": err.Error(),
-		}
-		responseMsg, err := json.Marshal(errorMsg)
-		if err != nil {
-			http.Error(writerResponse, "Erro interno do servidor", http.StatusInternalServerError)
-			return
-		}
-		writerResponse.Write(responseMsg)
+		return apis.HTTPResponse{}, err
 	}
-	writerResponse.Write([]byte(response.Body))
+
+	apiResponse, err := apis.GetWeatherData(request, params)
+	if err != nil {
+		return apiResponse, err
+	}
+	return apiResponse, nil
 }
