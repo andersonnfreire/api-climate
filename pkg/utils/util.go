03383096524d3c8c224a0dc8c977e10a28df72ed
Anderson Freire <anderson_nascimento.16@hotmail.com>
2024-01-21 20:16:48 -0300

    feat: allow characteres specials in pdf

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -6,3 +6,13 @@ import "strings"
 func ToUpper(str string) string {
 	return strings.ToUpper(str)
 }
+
+func Utf8ToIso(s string) string {
+	var iso8859_1 []byte
+	for _, runeValue := range s {
+		if runeValue <= 0xFF {
+			iso8859_1 = append(iso8859_1, byte(runeValue))
+		}
+	}
+	return string(iso8859_1)
+}
