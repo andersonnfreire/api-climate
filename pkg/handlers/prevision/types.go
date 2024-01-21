7800dd7e95cf5776cbc50b0fcd0fcb94dc2a1ddc
Anderson Freire <anderson_nascimento.16@hotmail.com>
2024-01-21 20:17:16 -0300

    feat: convert values api weather for api internal

--- pkg/handlers/prevision/types.go
+++ pkg/handlers/prevision/types.go
@@ -1,15 +1,11 @@
 package prevision
 
 type WeatherForecastsResponse struct {
-	Coordinates         Coordinates `json:"coord"`
-	ClimaticWeather     []Weather   `json:"weather"`
-	InfoMain            InfoMain    `json:"main"`
-	InfoWind            InfoWind    `json:"wind"`
-	InfoClouds          InfoClouds  `json:"clouds"`
-	TimeDataCalculation int         `json:"dt"`
-	Sys                 InfoCountry `json:"sys"`
-	Name                string      `json:"name"`
-	Timezone            int         `json:"timezone"`
+	Coordinates     Coordinates `json:"coord"`
+	ClimaticWeather []Weather   `json:"weather"`
+	InfoMain        InfoMain    `json:"main"`
+	InfoClouds      InfoClouds  `json:"clouds"`
+	Name            string      `json:"name"`
 }
 
 type Coordinates struct {
@@ -25,27 +21,13 @@ type Weather struct {
 }
 
 type InfoMain struct {
-	Temp      float64 `json:"temp"`
-	FeelsLike float64 `json:"feels_like"`
-	TempMin   float64 `json:"temp_min"`
-	TempMax   float64 `json:"temp_max"`
-	Pressure  int64   `json:"pressure"`
-	Humidity  int64   `json:"humidity"`
-}
-
-type InfoWind struct {
-	Speed float64 `json:"speed"`
-	Deg   int     `json:"deg"`
+	Temp     float64 `json:"temp"`
+	TempMin  float64 `json:"temp_min"`
+	TempMax  float64 `json:"temp_max"`
+	Pressure int64   `json:"pressure"`
+	Humidity int64   `json:"humidity"`
 }
 
 type InfoClouds struct {
-	All int `json:"all"`
-}
-
-type InfoCountry struct {
-	Type    int    `json:"type"`
-	ID      int    `json:"id"`
-	Country string `json:"country"`
-	Sunrise int    `json:"sunrise"`
-	Sunset  int    `json:"sunset"`
+	All int64 `json:"all"`
 }
