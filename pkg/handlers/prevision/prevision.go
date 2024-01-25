4485679241ad19deb02f913382ece85408c80edc
Anderson Freire <anderson_nascimento.16@hotmail.com>
2024-01-25 09:59:09 -0300

    chore: validate params and structure code

--- pkg/handlers/prevision/prevision.go
+++ pkg/handlers/prevision/prevision.go
@@ -4,25 +4,50 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/andersonnfreire/api-climate/pkg/apis"
 	"github.com/andersonnfreire/api-climate/pkg/config"
-	"github.com/andersonnfreire/api-climate/pkg/handlers/validation"
+	"github.com/go-playground/validator"
 )
 
+var validate = validator.New()
+
 func GetPrevisionHandler(writerResponse http.ResponseWriter, request *http.Request, cfg config.Config) (*WeatherForecastsResponse, error) {
-	params, err := validation.ValidateQueryParamGetPrevision(request)
-	if err != nil {
+
+	params := ParamsGetPrevision{
+		Token:  request.URL.Query().Get("token"),
+		Cidade: request.URL.Query().Get("cidade"),
+	}
+
+	if err := validate.Struct(params); err != nil {
 		return nil, err
 	}
 
-	apiResponse, err := apis.GetWeatherData(request, params)
+	options := apis.HTTPRequestOptions{
+		Method: http.MethodGet,
+		URL:    "http://api.openweathermap.org/data/2.5/weather",
+		QueryParams: map[string]string{
+			"appid": params.Token,
+		},
+		Timeout: 5 * time.Minute,
+	}
+
+	if params.Cidade != "" {
+		options.QueryParams["cidade"] = params.Cidade
+	}
+
+	if params.Language != "" {
+		options.QueryParams["lang"] = params.Language
+	}
+
+	response, err := apis.SendHTTPRequest(options)
 	if err != nil {
 		return nil, err
 	}
 
 	var weatherData *WeatherForecastsResponse
-	if err := json.Unmarshal(apiResponse.Body, &weatherData); err != nil {
+	if err := json.Unmarshal(response.Body, &weatherData); err != nil {
 		return nil, fmt.Errorf("erro ao decodificar dados da API: %s", err.Error())
 	}
 
